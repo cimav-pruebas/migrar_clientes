8e5c3a5002a05b8b84a8c6d1aed24b070a2d42d2
Juan Calderon <juan.calderon@gmail.com>
2015-02-13 13:08:33 -0700

    Clientes Listo. Falta Tamano_Empresa y Contactos.

--- src/migrarclientes/MigrarClientes.java
+++ src/migrarclientes/MigrarClientes.java
@@ -36,11 +36,17 @@ public class MigrarClientes {
             DriverManager.registerDriver(oraclePostgres);
 
             Connection connOracle = DriverManager.getConnection("jdbc:oracle:thin:@//10.1.0.44:1521/cimavXDB.netmultix.cimav.edu.mx", "almacen", "afrika");
-                                                               //jdbc:oracle:thin:@//10.1.0.44:1521/cimavXDB.netmultix.cimav.edu.mx
-            Connection connPostgres = DriverManager.getConnection("jdbc:postgresql://10.0.4.40:5432/rh_development", "rh_user", "rh_1ser");
+            Connection connPostgres = DriverManager.getConnection("jdbc:postgresql://localhost:5432/sigre_development");
 
             try (Statement stmtOra = connOracle.createStatement(); Statement stmtPost = connPostgres.createStatement()) {
-                String sqlClientes = "SELECT * FROM CL01";
+                
+                /****************/
+                stmtPost.execute("DELETE from vinculacion_clientes");
+                stmtPost.execute("ALTER SEQUENCE vinculacion_clientes_id_seq RESTART WITH 1");
+                /****************/
+                
+                String sqlClientes = "SELECT * FROM CL01 c1 JOIN CL13 c13 ON c1.CL01_CIUDAD = c13.CL13_CIUDAD";
+                
                 ResultSet rsOraClientes = stmtOra.executeQuery(sqlClientes);
                 
                 while(rsOraClientes.next()){
@@ -50,7 +56,7 @@ public class MigrarClientes {
                     String calle = rsOraClientes.getString("CL01_CALLE").trim();
                     String colonia = rsOraClientes.getString("CL01_COLONIA").trim();
                     Integer idCiudad = rsOraClientes.getInt("CL01_CIUDAD");
-                    String pais = rsOraClientes.getString("CL01_Pais").trim();
+                    String idPais = rsOraClientes.getString("CL01_Pais").trim();
                     String postal = rsOraClientes.getString("CL01_postal").trim();
                     String lada = rsOraClientes.getString("CL01_lada").trim();
                     String tel1 = rsOraClientes.getString("CL01_telefono1").trim();
@@ -66,18 +72,33 @@ public class MigrarClientes {
                     String num_int = rsOraClientes.getString("CL01_no_int").trim();
                     Integer idEstado = rsOraClientes.getInt("CL01_estado");
                     
+                    String ciudad = rsOraClientes.getString("CL13_nombre").trim();
+                    
                     String nombre = nom1 + nom2;
                     calle = calle + " " + num_ext + " " + num_int;
                     if (!localidad.isEmpty()) colonia = colonia + ", " + localidad;
                     String telefono = lada + " " + tel1 + " " + tel2;
                     fax = lada + " " + fax;
                     
+                    idTipoCliente = idTipoCliente == 0 ? 6 : idTipoCliente; // tipoCliente es el SECTOR :: 1EMPRESA, 2SECTOR EDUCATIVO, 3CENTRO..., 4PUBLi..., 5MAQUILA    0y6 son Ninugno
                     
-                    String slqMigration = clave + ", " + contacto;
+                    String sqlMigration = "clave, nombre, rfc, razon_social, num_empleados, calle_num, colonia, cp, telefono, fax, created_at, tamano_empresa, sector, pais_id, estado_id, ciudad";
+                    String sqlValues = "'" + clave.trim() + "', '" + nombre + "', '" + rfc.trim() + "', '" + nombre  + "', " + numEmp + ", '" + calle + "', '" + colonia + "', '" + postal.trim() + "', '" 
+                            + telefono.trim() + "', '" + fax.trim() + "', now(), " + idTipoEmpresa  + ", " + idTipoCliente + ", " + idPais + ", " + idEstado + ", '" + ciudad + "'";
                     
-                    if (contacto.trim().length() > 0)
-                        System.out.println("" + slqMigration);
+                    sqlMigration = "INSERT INTO vinculacion_clientes (" + sqlMigration + ") VALUES (" + sqlValues + ");";
+                            
+                    System.out.println("" + sqlMigration);
 
+                    /****************/
+                    stmtPost.execute(sqlMigration);
+                    /****************/
+                    
+// select table_name, column_name from dba_tab_columns where column_name like '%CIUDAD%';
+                    
+// INSERT INTO "public".vinculacion_clientes (rfc, razon_social, num_empleados, calle_num, colonia, cp, telefono, fax, email, created_at, updated_at, tamano_empresa, sector, pais_id, estado_id, ciudad, clave, nombre) 
+// vALUES ('COGJ750304QN3', 'JESUS MIGUEL COBOS GALLARDO', 0, 'FILTEPEC 414 ', 'CAFETALES , CHIHUAHUA', '31125', '  ', ' ', NULL, NULL, NULL, 0, 1, 1, 8, 'CHIHUAHUA', '00445', 'JESUS MIGUEL COBOS GALLARDO');
+                    
                 }
                 
             } catch (Exception e2) {
